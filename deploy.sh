6670ec1d653fc021243c7c104f2a71636c2829e3
Pankaj Badgujar <pbadguarns@netskope.com>
2020-04-25 19:35:32 -0700

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-#we need to generate two builds one for latest tag and other for unique version using SHA ( fed via env variable in travis.ym)
-#this is required so that the images on the K8s can be updated. If we have just latest tag then K8s would not able to distinguish 
-#images 
 docker build -t miththoobhai/multi-client:latest -t miththoobhai/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t miththoobhai/multi-server:latest -t miththoobhai/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t miththoobhai/multi-worker:latest -t miththoobhai/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
@@ -15,5 +12,5 @@ docker push miththoobhai/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=miththoobhai/multi-server:$SHA
-kubectl set image deployments/client-deployment server=miththoobhai/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=miththoobhai/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=miththoobhai/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=miththoobhai/multi-worker:$SHA
